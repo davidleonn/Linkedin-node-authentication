24a11519da61cb1fcba253810cce363fc4992b57
David León <davidleon_06@hotmail.com>
2022-07-19 10:48:31 +0200

    a

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     'linkedinAuth': {
-      'clientID': '7701hpbqzajkmc', // your App ID
-      'clientSecret': 'iZOMnDk0sROh9aje', // your App Secret
+      'clientID': '7701hpbqzajkmc', // El ID de la app de linkedin
+      'clientSecret': 'iZOMnDk0sROh9aje', // El "secreto" creado en la app de linkedin
       'callbackURL': 'https://matias.zito.com.ar/'
     }
   }
